bb83107800cfe9d12764d9363439a90004485b0a
Umut Arslan <arslanumut03@gmail.com>
2025-07-21 15:14:03 +0300

    some c projects

--- compound_calculator/main.c
+++ compound_calculator/main.c
@@ -25,7 +25,7 @@ int main() {
     printf("Surface Area: %0.2lf\n", surfaceArea);
     printf("Volume: %0.2lf\n", volume);
 
-    */
+    
 
 
     double principal = 0.0;
@@ -51,6 +51,139 @@ int main() {
 
     total = principal * pow((1 + (rate / timesCompounded)), (timesCompounded * years));
     printf("After %d years, the total amount will be $%0.2lf \n", years, total);
+
+    
+
+
+    int age = 0;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+
+    if (age >= 65)
+    {
+        printf("You are an elder.\n");
+    }
+    else if (age >= 18)
+    {
+        printf("You are an adult.\n");
+    
+    }
+    else if (age >= 13 && age < 18)
+    {
+        printf("You are a teenager.\n");
+    }   
+    else
+    {
+        printf("You are a child.\n");
+    }
+
+    
+
+
+    bool isStudent = false;
+
+    if (isStudent == true)
+    {
+        printf("You are a student.\n");
+    }
+    else{
+        printf("You are not a student.\n");
+    }
+
+    
+
+    char name[50] = "";
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';  
+
+    if (strlen(name)== 0)
+    {
+        printf("You did not enter a name.\n");
+    }
+    else{
+        printf("Hello, %s", name);
+    
+    }
+
+    
+
+
+    int choice = 0;
+    float pounds = 0.0f;
+    float kilograms = 0.0f;
+
+    printf("Weight Conversion Calculator\n");
+    printf("1. Kilograms to Pounds\n");
+    printf("2. Pounds to Kilograms\n");
+    printf("Enter your choice (1 or 2): ");
+    scanf("%d", &choice);
+    
+    if (choice == 1)
+    {
+        printf("Enter weight in kilograms: ");
+        scanf("%f", &kilograms);
+        pounds = kilograms * 2.20462;
+        printf("%.2f kilograms is equal to %.2f pounds.\n", kilograms, pounds);
+
+    }
+    else if (choice ==2)
+    {
+        printf("Enter weight in pounds: ");
+        scanf("%f", &pounds);
+        kilograms = pounds / 2.20462;
+        printf("%.2f pounds is equal to %.2f kilograms.\n", pounds, kilograms);
+    }
+    else{
+        printf("Invalid choice. Please enter 1 or 2.\n");
+        
+    }
+
+    */
+
+
+
+    char choice = '\0';
+    float fahrenheit = 0.0f;
+    float celsius = 0.0f;
+
+
+    printf("Temperature Conversion Calculator\n");
+    printf("Enter 'C' to convert Celsius to Fahrenheit: \n");
+    printf("Enter 'F' to convert Fahrenheit to Celsius: ");
+    printf("Enter your choice (C or F): ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("Enter temperature in Celsius: ");
+        scanf("%f", &celsius);
+        fahrenheit = (celsius * 9 / 5) + 32;
+        printf("%.1f Celsius is equal to %.1f Fahrenheit.\n", celsius, fahrenheit);
+        
+    }
+    else if (choice == 'F')
+    {
+        printf("Enter temperature in Fahrenheit: ");
+        scanf("%f", &fahrenheit);
+        celsius = (fahrenheit - 32) * 5 / 9;
+        printf("%.1f Fahrenheit is equal to %.1f Celsius.\n", fahrenheit, celsius);
+    }
+    else
+    {
+        printf("Invalid choice. Please enter 'C' or 'F'.\n");
+
+    }
+    
+    
+    
+    
+    
+    
+    
     
     return 0;
 }
